933ff49993da4ae708f46ef0342eb99fe0a2c615
Eliran D <elirandav@gmail.com>
2018-06-02 16:43:58 +0300

    First commit

---
+++ src/main/java/com/example/springAsyncServlet/SpringAsyncServletApplication.java
@@ -0,0 +1,12 @@
+package com.example.springAsyncServlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsyncServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsyncServletApplication.class, args);
+	}
+}
